22ae20a7caae02dae5ba70797f1a9f346a8caa7d
AlvinKimata <alvinkimata@gmail.com>
2023-09-21 15:48:02 +0300

    implement DenseNet MoE

--- train_moe.py
+++ train_moe.py
@@ -74,3 +74,133 @@ class _DenseBlock(nn.Module):
             features.append(new_features)
         return torch.cat(features, 1)
 
+
+class DenseNet(MoEBase):
+    r"""Densenet-BC model class, based on
+    `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>`
+    Args:
+        growth_rate (int) - how many filters to add each layer (`k` in paper)
+        block_config (list of 3 or 4 ints) - how many layers in each pooling block
+        num_init_features (int) - the number of filters to learn in the first convolution layer
+        bn_size (int) - multiplicative factor for number of bottle neck layers
+            (i.e. bn_size * k features in the bottleneck layer)
+        drop_rate (float) - dropout rate after each dense layer
+        num_classes (int) - number of classification classes
+        small_inputs (bool) - set to True if images are 32x32. Otherwise assumes images are larger.
+        efficient (bool) - set to True to use checkpointing. Much more memory efficient, but slower.
+    """
+
+    def __init__(self, growth_rate=12, block_config=(16, 16, 16), compression=0.5,
+                 num_init_features=24, bn_size=4, drop_rate=0,
+                 num_classes=10, small_inputs=True, efficient=False, ratio=1.0, n_expert=2):
+
+        super(DenseNet, self).__init__()
+        assert 0 < compression <= 1, 'compression of densenet should be between 0 and 1'
+        conv_layer = MoEConv
+        linear_layer = nn.Linear
+
+        # First convolution
+        if small_inputs:
+            self.features = nn.Sequential(OrderedDict([
+                ('conv0', nn.Conv2d(3, num_init_features, kernel_size=3, stride=1, padding=1, bias=False)),
+            ]))
+        else:
+            self.features = nn.Sequential(OrderedDict([
+                ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2, padding=3, bias=False)),
+            ]))
+            self.features.add_module('norm0', nn.BatchNorm2d(num_init_features))
+            self.features.add_module('relu0', nn.ReLU(inplace=True))
+            self.features.add_module('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1,
+                                                           ceil_mode=False))
+
+        # Each denseblock
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            block = _DenseBlock(
+                conv_layer=conv_layer,
+                num_layers=num_layers,
+                num_input_features=num_features,
+                bn_size=bn_size,
+                growth_rate=growth_rate,
+                drop_rate=drop_rate,
+                efficient=efficient,
+                n_expert=n_expert,
+                ratio=ratio,
+            )
+            self.features.add_module('denseblock%d' % (i + 1), block)
+            num_features = num_features + num_layers * growth_rate
+            if i != len(block_config) - 1:
+                # transition layer remains the same
+                trans = _Transition(num_input_features=num_features,
+                                    num_output_features=int(num_features * compression))
+                self.features.add_module('transition%d' % (i + 1), trans)
+                num_features = int(num_features * compression)
+
+        # Final batch norm
+        self.features.add_module('norm_final', nn.BatchNorm2d(num_features))
+
+        # Linear layer
+        self.classifier = linear_layer(num_features, num_classes)
+
+        # Initialization
+        for name, param in self.named_parameters():
+            if 'conv' in name and 'weight' in name:
+                n = param.size(0) * param.size(2) * param.size(3)
+                param.data.normal_().mul_(math.sqrt(2. / n))
+            elif 'norm' in name and 'weight' in name:
+                param.data.fill_(1)
+            elif 'norm' in name and 'bias' in name:
+                param.data.fill_(0)
+            elif 'classifier' in name and 'bias' in name:
+                param.data.fill_(0)
+
+    def forward(self, x):
+        if self.router is not None:
+            self.set_score(self.router(x))
+        features = self.features(x)
+        out = F.relu(features, inplace=True)
+        out = F.adaptive_avg_pool2d(out, (1, 1))
+        out = torch.flatten(out, 1)
+        out = self.classifier(out)
+        return out
+
+
+def densenet_cifar_moe(**kwargs):
+    total_depth = 40
+    if (total_depth - 4) % 3:
+        raise Exception('Invalid depth')
+    block_layer_config = [(total_depth - 4) // 6 for _ in range(3)]
+    return DenseNet(block_config=block_layer_config, growth_rate=12, efficient=False, small_inputs=True, **kwargs)
+
+
+def densenet_imagenet_moe(**kwargs):
+    total_depth = 40
+    if (total_depth - 4) % 3:
+        raise Exception('Invalid depth')
+    block_layer_config = [(total_depth - 4) // 6 for _ in range(3)]
+    return DenseNet(block_config=block_layer_config, growth_rate=12, efficient=False, small_inputs=False, **kwargs)
+
+
+if __name__ == "__main__":
+    # To test this code, comment out the self.set_score(self.router(x)) in the forward path
+    depth = 40
+    if (depth - 4) % 3:
+        raise Exception('Invalid depth')
+    block_config = [(depth - 4) // 6 for _ in range(3)]
+    print(block_config)
+    growth_rate = 12
+
+    model = DenseNet(
+        growth_rate=growth_rate,
+        block_config=block_config,
+        num_init_features=growth_rate * 2,
+        num_classes=10,
+        small_inputs=False,
+        efficient=False,
+        ratio=0.2,
+        n_expert=3
+    )
+
+    scores = torch.randn([100, 3])
+    model.set_score(scores)
+    print(model(torch.randn(100, 3, 224, 224)).shape)
