55831a64f75bfe2deb85e1309a90fff7f12c3549
AlvinKimata <alvinkimata@gmail.com>
2023-09-22 13:17:42 +0300

    add webapp project

---
+++ frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace school_project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
