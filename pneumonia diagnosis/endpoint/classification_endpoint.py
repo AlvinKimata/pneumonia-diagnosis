b701c326ba3808654ccbd92be8ed528b09bd046a
AlvinKimata <alvinkimata@gmail.com>
2023-10-04 10:48:43 +0300

    add js code for toggling sidebar

--- pneumonia diagnosis/endpoint/classification_endpoint.py
+++ pneumonia diagnosis/endpoint/classification_endpoint.py
@@ -1,24 +1,17 @@
 import sys
-import os
-import torch
 import pathlib
 import traceback
 from flask import Flask, request, jsonify
-import numpy as np
-import cv2
-
-
 
 #Setting path.
 cwd = pathlib.Path(__name__).parent.resolve()
 cwd = str(cwd).split(sep = '/')[:-1]
 cwd = '/'.join(cwd)
-
 sys.path.append(cwd)
 print(cwd)
+
+
 import utils
-
-
 app = Flask(__name__)
 
 @app.route('/classification', methods = ['POST'])
@@ -28,6 +21,7 @@ def predict_class():
             #Check request content type.
             content_type = request.content_type
             if content_type == "image/jpeg":
+
                 #Read image binary data.
                 image_data = request.data
                 prediction = utils.image_classification(image_data, model = model)
