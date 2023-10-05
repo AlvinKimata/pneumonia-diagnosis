6151fdd46beb4325695f95890f32e3c507f43330
AlvinKimata <alvinkimata@gmail.com>
2023-10-05 12:17:54 +0300

    Rename project model to user model

---
+++ frontend/Models/User.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace school_project.Models
+{
+    public class Employee{
+        public int Id {get; set;}
+
+        [Required]
+        public string Name {get; set;}
+
+        [Required]
+        public string Email {get; set;}
+
+        public Dept? Department {get; set;}
+
+        public string PhotoPath {get; set;}
+
+    }
+}
