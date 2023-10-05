5983985ffb38a1a3a49d266df8798ebddf95a0ed
AlvinKimata <alvinkimata@gmail.com>
2023-10-05 17:59:41 +0300

    add login and registration views

--- webapp/ViewModels/RegisterViewModel.cs
+++ webapp/ViewModels/RegisterViewModel.cs
@@ -10,6 +10,9 @@ namespace school_project.ViewModels
     public class RegisterViewModel
     {
         [Required]
+        public string Name {get; set;}
+        
+        [Required]
         [EmailAddress]
         public string Email { get; set; }
 
