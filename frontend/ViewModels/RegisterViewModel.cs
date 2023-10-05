ab0e4deb1c8af3e15bc6df7e12c06765573dac57
AlvinKimata <alvinkimata@gmail.com>
2023-10-05 12:35:25 +0300

    Add registration viewmodel

---
+++ frontend/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace school_project.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "password and confirmation passworddo not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string City { get; set; }
+    }
+}
