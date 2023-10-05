7f9523a260fff03c174b4cae3bb445d2952d1d26
AlvinKimata <alvinkimata@gmail.com>
2023-10-05 12:38:38 +0300

    Add login viewmodel

---
+++ frontend/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace school_project.Models{
+    public class LoginViewModel{
+        [Required]
+        [EmailAddress]
+        public string Email {get; set;}
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password {get; set;}
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe {get; set;}
+    }
+}
