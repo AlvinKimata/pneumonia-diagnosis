c44cf0b5280ddbd4433d75643fea08d297a343e3
AlvinKimata <alvinkimata@gmail.com>
2023-10-09 15:10:25 +0300

    add single image diagnosis model attributes

---
+++ webapp/Models/SingleImageInferenceProject.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace school_project.Models
+{
+    public class SingleImageDiagnosis{
+        public int id {get; set;}
+
+        public string PhotoPath {get; set;}
+
+        //The label of the image after model inference.
+        public string ImageResult {get; set;}
+    }
+}
