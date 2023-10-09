4ad62ad961e047a25a1048b5fb85c7777dd98d76
AlvinKimata <alvinkimata@gmail.com>
2023-10-09 16:02:31 +0300

    Add SingleImageDiagnosis viewmodel

---
+++ webapp/ViewModels/SingleImageDiagnosisViewModel.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace school_project.ViewModels
+{
+    public class SingleImageDiagnosis
+    {
+        [Required]
+        public string PhotoPath
+    }
+}
