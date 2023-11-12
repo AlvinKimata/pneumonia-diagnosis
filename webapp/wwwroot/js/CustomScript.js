4ce06e9677e8f25146fadd83e332404df2488b9e
AlvinKimata <alvinkimata@gmail.com>
2023-11-12 21:41:28 +0300

    add custom js script for confirmDelete

---
+++ webapp/wwwroot/js/CustomScript.js
@@ -0,0 +1,15 @@
+﻿function confirmDelete(uniqueId, isDeleteClicked) {
+    var deleteSpan = 'deleteSpan_' + uniqueId;
+    var confirmDeleteSpan = 'confirmDeleteSpan_' + uniqueId;
+
+    if (isDeleteClicked) {
+        //Hide the delete span element.
+        $('#' + deleteSpan).hide();
+        $('#' + confirmDeleteSpan).show();
+    }
+    else {
+        //Show the delete span element.
+        $('#' + deleteSpan).show();
+        $('#' + confirmDeleteSpan).hide();
+    }
+}
