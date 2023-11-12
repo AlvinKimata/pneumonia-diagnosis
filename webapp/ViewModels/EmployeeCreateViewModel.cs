0d612025dc940ee3bca4b36e61f0716619e644f6
AlvinKimata <alvinkimata@gmail.com>
2023-11-12 19:42:26 +0300

    add edit user view model

---
+++ webapp/ViewModels/EmployeeCreateViewModel.cs
@@ -0,0 +1,27 @@
+﻿using school_project.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace school_project.ViewModels
+{
+    public class EmployeeCreateViewModel
+    {
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+        ErrorMessage = "Invalid email format")]
+
+        [Display(Name = "Office email")]
+        public string Email { get; set; }
+        [Required]
+        public Dept? Department { get; set; }
+
+        public List<IFormFile> Photos { get; set; }
+    }
+}
