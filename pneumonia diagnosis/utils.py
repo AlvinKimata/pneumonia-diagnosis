d463effc99ce1a5d2b191ace15fcbd17ef0e07df
AlvinKimata <alvinkimata@gmail.com>
2023-12-18 22:44:49 +0300

    push changes

--- pneumonia diagnosis/utils.py
+++ pneumonia diagnosis/utils.py
@@ -15,7 +15,7 @@ def load_model():
     model = model.to(device)
 
     # Load model ckpt.
-    ckpt = torch.load('..\\ckpt\\model.pth', map_location = device) #Load model using an endpoint.
+    ckpt = torch.load('..\\ckpt\\model_weights.pth', map_location = device) #Load model using an endpoint.
     model.load_state_dict(ckpt, strict = True)
 
     #Evaluation mode.
@@ -37,14 +37,14 @@ def image_classification(image_bn, model):
     grads = model.forward(img)
     out = grads.detach().cpu().numpy()
     out = np.round(out * 100, 2)
-    if out > 0.5:
-        print(out)
-        return f"{out}% probability of prescence of pneumonia."
+    # if out > 0.5:
+    #     print(out)
+    return f"{out}% probability of prescence of pneumonia."
     
-    else:
-        out = 1 - out
-        print(out)
-        return f"{out}% probability of abscence of pneumonia."
+    # else:
+    #     # out = 1 - out
+    #     print(out)
+    #     return f"{out}% probability of abscence of pneumonia."
 
 
 def decode_image_binary(image_bn):
